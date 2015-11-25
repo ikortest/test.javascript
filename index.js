f5b805d2d7cd3db6ab4046fc6bf0559efae796e0
Ilya Korolev <notikor@yahoo.com>
2015-11-25 15:52:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,39 @@ var express = require('express');
 var expSess = require("express-session");
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
+
 
 // support json encoded bodies
 app.use(bodyParser.json());
 // support encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Start the app and visit the following URLs
+// Path Injection - http://localhost:3000/..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fetc
+
+function getUserHomeDirectory(username) {
+    console.log("Resolving " + username + " to home directory");
+    return path.resolve(__dirname, 'home', username);
+}
+
+app.get('/path/:username', function(req, res) {
+    var username = req.params.username;
+    res.send('Your home directory is located at: ' + getUserHomeDirectory(username));
+});
+
+app.get('/safepath/:username', function(req, res) {
+    var username = req.params.username;
+
+    // Valid username contains Alpha-Numeric characters, is at least 3 characters long but less than 16
+    var re = /^[A-Za-z0-9]{3,16}$/g;
+
+    if (re.test(username)) {
+        res.send('Your home directory is located at: ' + getUserHomeDirectory(username));
+    } else {
+        res.send('Invalid username');
+    }
+});
 
 // ======= for httpOnly session test =======
 require('./httponlysession/test.explicitly-set-httponly')(app, expSess);
